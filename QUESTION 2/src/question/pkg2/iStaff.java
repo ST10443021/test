5485aee021c59b5a40da4ccb13e991db8a10aab5
ST10443021 <166050654+ST10443021@users.noreply.github.com>
2024-09-28 16:44:31 +0300

    initial commit

---
+++ QUESTION 2/src/question/pkg2/iStaff.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package question.pkg2;
+
+/**
+ *
+ * @author lab_services_student
+ */
+interface iStaff {
+    //Methods
+    public int getStaffNumber();
+    
+    public String getStaffLocation();
+    
+    public String getStaffHiringProcess();
+}
